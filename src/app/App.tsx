8c2420fbcbb1279a61ff45b12bc44049cc17ba7e
Axel Froborg <axel.froborg@gmail.com>
2021-01-27 21:22:02 +0100

    foundation done

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Question from '../components/Question';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Question />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Question.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import q from '../questions.json';
+import styles from './question.module.scss';
+
+const Question = () => {
+  const [question, setQuestion] = useState('');
+  const [questions, setQuestions] = useState<string[]>(q);
+
+  useEffect(() => {
+    newQuestion();
+  }, []);
+
+  const newQuestion = () => {
+    const idx = random(questions.length);
+    setQuestion(questions[idx]);
+
+    const temp = [...questions];
+    temp.splice(idx, 1);
+
+    setQuestions(temp);
+  };
+
+  const random = (max: number) => Math.floor(Math.random() * max);
+
+  return (
+    <div className={styles.container} onClick={newQuestion}>
+      <h1>Jag har aldrig</h1>
+      <h2>{question}</h2>
+      <p>Klicka varsomhelst för att gå vidare till nästa fråga.</p>
+    </div>
+  );
+};
+
+export default Question;
